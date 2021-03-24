ca6aa600eb203a4903e41951b84e40de522927b5
Jinshi Sai <jinshi@Jinshis-MacBook-Pro.local>
2021-03-24 18:59:03 +0800

    Add a new function, plot_model.

--- ptsmodel/__init__.py
+++ ptsmodel/__init__.py
@@ -1,5 +1,6 @@
 # import model
 from ._ptsmodel import PTSMODEL
 from . import export_radmc_tofits
+from . import plot_model
 
-__all__ = ['PTSMODEL', 'export_radmc_tofits']
+__all__ = ['PTSMODEL', 'export_radmc_tofits', 'plot_model']
