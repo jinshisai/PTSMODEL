Fixed a bug for continuum calculation.

ptsmodel/_ptsmodel.py
@@ -70,7 +70,7 @@ class PTSMODEL():
 
         # reading file
         # grid
-        f                = 'amr_grid.inp'
+        f = 'amr_grid.inp'
         if os.path.exists(f) == False:
             print ('ERROR\tread_model: amr_grid.inp cannot be found.')
             return
@@ -114,7 +114,7 @@ class PTSMODEL():
 
         # density
         # dust denisty [g/cm3]
-        f         = 'dust_density.inp'
+        f = 'dust_density.inp'
         if os.path.exists(f) == False:
             print ('WARNING\t: dust_density.inp cannot be found. Put zero for dust density.')
             rho_d = np.zeros(arraysize)
@@ -282,7 +282,7 @@ class PTSMODEL():
         '''
         Calculate Sigma 0, column density of the disk at 1 au, from Mdisk.
         Disk is a cut-off disk.
-        Mdisk = 2 pi int_rin^rout Sig0 (r/1 au)^(-p) dr
+        Mdisk = 2 pi int_rin^rout Sig0 (r/1 au)^p r dr
 
         Args:
             mdisk: disk mass (g)

ptsmodel/export_radmc_tofits.py
@@ -133,9 +133,11 @@ def export_radmc_tofits(outname, f='image.out', obsinfo='obsinfo.txt', hdr=None,
     Export a radmc output file into a fits file.
 
     '''
+    # print
+    print ('Export a radmc image to a fits file.')
+
     # reading file
     print ('reading files...')
-    f       = 'image.out'
     iformat = np.genfromtxt(f, dtype=None, max_rows=1)
     imsize  = np.genfromtxt(f, delimiter='     ',max_rows=1, skip_header=1, dtype=int)
     nlam    = np.genfromtxt(f, max_rows=1, skip_header=2, dtype=int)
@@ -267,8 +269,8 @@ def export_radmc_tofits(outname, f='image.out', obsinfo='obsinfo.txt', hdr=None,
     hdr['CRPIX2']   = ny//2
     hdr['CUNIT2']   = 'deg'
     hdr['CTYPE3']   = 'FREQ'
-    hdr['CRVAL3']   = freq[0]+freq_vsys
-    hdr['CDELT3']   = freq[1] - freq[0]
+    hdr['CRVAL3']   = freq[0]+freq_vsys if nlam >= 2 else freq
+    hdr['CDELT3']   = freq[1] - freq[0] if nlam >= 2 else 1.
     hdr['CRPIX3']   = 1.000000000000E+00
     hdr['CUNIT3']   = 'Hz'
     hdr['CTYPE4']   = 'STOKES'